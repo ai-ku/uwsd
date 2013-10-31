7ce3c0719e0c75cbe569f3702cf3c3b659fa4e3f
osmanbaskaya <obaskaya@ku.edu.tr>
2013-11-01 01:59:47 +0200

    all-word sentence fetching scripts done

---
+++ run/fetch-semeval2010-aw.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = "Osman Baskaya"
+
+
+from bs4 import BeautifulSoup
+
+f = '../data/semeval2010/aw/test/English/EnglishAW.test.xml'
+soup = BeautifulSoup(open(f), 'xml')
+
+sentences = soup.find_all('s')
+for sentence in sentences:
+    print u' '.join(sentence.text.split()).encode('utf-8').strip()
+
