87b53ae604c01130b7aa763a7bec56f98fbaefef
osmanbaskaya <obaskaya@ku.edu.tr>
2013-11-07 10:51:42 +0200

    for local update

--- run/find-sm-with-oracle.py
+++ run/find-sm-with-oracle.py
@@ -8,13 +8,69 @@ __author__ = "Osman Baskaya"
     Algorithm 2.
 """
 
-import nltk.corpora import wordnet as wn
+from nltk.corpus import wordnet as wn
 import sys
+from nlp_utils import fopen
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 5:
     msg = "Usage: {} key_file number_of_semantic_class"
     print >> sys.stderr, msg.format(sys.argv[0])
     exit(1)
 
-key_file = sys.argv[1]
-n = int(sys.argv[2]) # number of semantic class we need to create
+class Semantic_Class(object):
+    
+    def __init__(self):
+        senses = set()
+        lemma_dict = dict()
+
+    def has_sense(self, sense):
+        return sense in self.senses
+
+    def add_synset(self, synset):
+        """Add the synset and its all descendants"""
+        #TODO: add all lemmas and its descendant lemmas from senses set
+        pass
+
+    def remove_synset(self):
+        """Removes the synset and its all descendants"""
+        #TODO: remove all lemmas and its descendant lemmas from senses set
+        pass
+
+def read_all_words(key_file):
+    with open(key_file) as f:
+        wi = []
+        for line in f:
+            line = line.split()
+            inst_id = line[1]
+            word = line[2].split('%')[0]
+            wi.append((inst_id, word, line[2]))
+    return wi
+
+def filter_by_pos(wi, pos, pos_file):
+    t = []
+    for line in fopen(pos_file):
+        line = line.split()
+        t.append((line[0], line[4].lower()[0])) #inst_id, pos-tag
+    d = dict(t)
+    return [(inst_id, word, key) for inst_id, word, key in wi if d[inst_id] == pos]
+
+# (word, instance_id, key triple) list
+
+def find_synset_with_best_accurracy(wik, sc_list):
+    """According to oracle accuracy, find the best synset to create a new 
+       semantic class """
+    pass
+
+def create_semantic_class():
+    pass
+
+
+def run():
+    key_file = sys.argv[1]
+    pos_file = sys.argv[2]
+    pos = sys.argv[3]
+    n = int(sys.argv[4]) # number of semantic class we need to create
+    wik = filter_by_pos(read_all_words(key_file), pos, pos_file)
+    create_semantic_class(wik)
+    for i in xrange(n):
+        find_synset_with_best_accuracy(wik, )
---
+++ run/nlp_utils.py
@@ -0,0 +1,78 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import division
+import math
+from collections import defaultdict as dd
+from itertools import izip
+import gzip
+
+__author__ = "Osman Baskaya"
+
+""" Utility functions that are used in experiments """
+
+def fopen(filename):
+    if filename.endswith('.gz'):
+        func = gzip.open
+    else:
+        func = open
+    return func(filename)
+
+
+def calc_perp(X, weight=None):
+    
+    d = dd(int)
+    if weight is None:
+        weight = [1] * len(X)
+
+    for tag, w in izip(X, weight):
+        d[tag] += w
+
+    total = sum(weight)
+    entropy = .0
+    for key in d.keys():
+        p = d[key] / total
+        entropy += -p * math.log(p, 2)
+    return 2 ** entropy
+
+def calc_perp_semeval(sense_list):
+    # sense list = [['t.1/0.8723', 't.6/0.0851', 't.50/0.0213', 't.18/0.0213'], ...]
+    senses = []
+    weight = []
+    for slist in sense_list:
+        m = [s.split('/') for s in slist]
+        for t in m:
+            senses.append(t[0])
+            if len(t) == 1:
+                weight.append(1.)
+            else:
+                weight.append(float(t[1]))
+
+    assert len(senses) == len(weight)
+    return calc_perp(senses, weight)
+
+def calc_perp_dict(d):
+    # test etmedim
+    entropy = 0.
+    tt = [(key, len(val)) for key, val in d.iteritems()]
+    total = sum([x[1] for x in tt])
+    for i, j in tt:
+        p = j / total
+        entropy += -p * math.log(p, 2)
+    return 2 ** entropy
+
+def calc_perp_dict_graded(d):
+    # test etmedim
+    entropy = 0.
+    tt = [(key, len(val)) for key, val in d.iteritems()]
+    total = sum([x[1] for x in tt])
+    for i, j in tt:
+        p = j / total
+        entropy += -p * math.log(p, 2)
+    return 2 ** entropy
+    
+
+#a = [1, 1, 1, 2, 2, 2, 3, 3]
+#b = ['a', 'a', 'a', 'b', 'b', 'b', 'c', 'c']
+#print calc_perp(a)
+#print calc_perp(b)
+
