598bbfeeede36fb88d8e3697cabf69b3ef5624f4
osmanbaskaya <obaskaya@ku.edu.tr>
2013-11-28 17:15:29 +0200

    various stats for ontonotes are ready

--- run/ontonotes-stats.py
+++ run/ontonotes-stats.py
@@ -6,7 +6,6 @@ __author__ = "Osman Baskaya"
 This module provides various statistics about Ontonotes.
 
 """
-
 import sys
 import os
 from bs4 import BeautifulSoup
@@ -14,7 +13,6 @@ import fnmatch
 from collections import defaultdict as dd
 from nltk.corpus.reader import BracketParseCorpusReader
 from itertools import count
-
 
 ONTO_PATH = sys.argv[1]
 
@@ -33,28 +31,30 @@ def find_files(topdir, pattern):
 
 def get_sense_mappings():
     
-    d = dd(lambda : count(0))
-    word_sense_dict = dd(dict)
+    version_dict = dd(lambda : count(0)) # keep tracking the annotation version
+    word_sense_dict = dd(dict) # keep tracking words' senses' version
     inventory_files = find_files(inventory, "*.xml")
-    num_processed = 0
     # ITA INF below: [#ofinstance, #total_ita_score, #of word <90, #total_score_for_<90]
     ita_inf = [0, 0, 0, 0] 
-    for f in inventory_files:
-        num_processed += 1
+    nsense = [0, 0]
+    for num_processed, f in enumerate(inventory_files):
         fn = os.path.basename(f).replace('.xml', '')
         if num_processed % 1000 == 0:
             print >> sys.stderr, "{} files processed".format(num_processed)
         soup = BeautifulSoup(open(f), 'xml')
         senses = soup.findAll('sense')
 
+        nsense[0] += len(senses)
+        nsense[1] += 1
+
         for sense in senses:
-            key = sense['n']
+            key = str(sense['n'])
             mapping = sense.findAll('mappings')[0]
             wn = mapping.findAll('wn')[0]
             version = wn['version']
             word_sense_dict[fn][key] = version
-            wn_senses = wn.text.split(',')
-            d[version].next()
+            wn_senses = wn.text.split(',') # maybe we can use it later
+            version_dict[version].next()
         ita = soup.findAll('ita') # inter-annotator agreement
         if len(ita) != 0:
             ita_inf[0] += 1
@@ -66,55 +66,84 @@ def get_sense_mappings():
 
     ita_inf[1] = ita_inf[1] / ita_inf[0] # averaging.for all instance ita score
     ita_inf[3] = ita_inf[3] / ita_inf[2] # averaging.ita score for word lower than 0.9
-    print ita_inf
-    print num_processed
+    print >> sys.stderr, "{} files processed (total)".format(num_processed)
+    print "ITA informations: {}".format(ita_inf)
+    avg_sense = nsense[0] / float(nsense[1])
+    print "total sense: {}, avg # of sense: {}".format(nsense[0], avg_sense)
+    version_list = [(key, val.next()) for key, val in version_dict.iteritems()]
+    version_list = sorted(version_list, key=lambda x: x[1], reverse=True)
+    with open('ontonotes-sensefreq-inventory.tab', 'w') as f:
+        for key, val in version_list:
+             f.write("{}\t{}\n".format(key, val))
+    return word_sense_dict
 
 def process_sense_annotation():
     
     print >> sys.stderr, "Sense Annotation processing started"
-    
-    d = dd(lambda : count(0))
-    pos_dict = dd(lambda : count(0))
+
+    word_sense_dict = get_sense_mappings()
+    sense_freq = dd(lambda : count(0)) # sense frequencies (wn3.0, wn2.0 etc) for annotation
+    word_freq = dd(lambda : count(0)) # words frequency in annontation
+    pos_dict = dd(lambda : count(0)) # pos distribution for annotation.
     num_adjudicated = 0 # Number of instance that adjudicated
     pattern = "*.sense"
     annotated_files = find_files(annotations_path, pattern)
-    for annotated_file in annotated_files:
-        fn = annotated_file.replace(annotations_path, "")
+    num_word_processed = 0 
+    for num_processed, annotated_file in enumerate(annotated_files):
+        #fn = annotated_file.replace(annotations_path, "")
         for line in open(annotated_file):
             line = line.split()
+            num_word_processed += +1
             if len(line) == 6:
                 num_adjudicated += 1
             word = line[3]
             pos_tag = word[-1]
             pos_dict[pos_tag].next()
             sense_tag = line[-1]
-            d[word].next()
-            #print fn, word, pos_tag, sense_tag
-    print pos_dict['n'].next(), pos_dict['v'].next()
-    exit()
+            word_freq[word].next()
 
+            version = word_sense_dict[word][sense_tag]
+            sense_freq[version].next()
+
+        if num_processed % 3000 == 0:
+            print >> sys.stderr, "{} files processed".format(num_processed)
+
+    ### Printing: Pos Info in annotated corpus  ###
+    num_noun = pos_dict['n'].next()
+    num_verb = pos_dict['v'].next()
+    pos_msg = "Noun\tVerb\tNoun+Verb\tTotalWord\n{}\t{}\t{}\t{}"
+    print pos_msg.format(num_noun, num_verb, num_verb + num_noun, num_word_processed)
+
+    ### Printing: Number of Adjudicated word
+    print "Number of adjudicated case: {}".format(num_adjudicated)
+
+    ### Writing: sense frequency in annotated data 
+    sensefreq_list = [(key, val.next()) for key, val in sense_freq.iteritems()]
+    sensefreq_list = sorted(sensefreq_list, key=lambda x: x[1], reverse=True)
+    with open('ontonotes-sensefreq-annotation.tab', 'w') as f:
+        for key, val in sensefreq_list:
+             f.write("{}\t{}\n".format(key, val))
     print >> sys.stderr, "Sense Annotation processing finished"
 
 def process_parse_annotation():
     reader = BracketParseCorpusReader(annotations_path, '.*parse')
+    pos_set = set("NN VB RB JJ".split()) # word level pos tags for n, v, adv, adj.
+    check_pos = lambda x: x in pos_set
+    d = dd(lambda: count(0))
     for fileid in reader.fileids():
         print fileid
         for sentence in reader.parsed_sents(fileid):
-            clean_sent_list = []
-            sent_list = []
-            pos_list = []
             for word, p in sentence.pos():
-                if p != '-NONE-':
-                    if word in fix:
-                        word = fix[word]
-                    clean_sent_list.append(word)
-                sent_list.append(word)
-                pos_list.append(p)
-            lines = [clean_sent_list, sent_list, pos_list]
-            print lines
+                pos = p[0:2]
+                if p != '-NONE-' and check_pos(pos):
+                     d[pos].next()
+        print d
         exit()
-            #write2file(lines, files)
-    pass
-process_parse_annotation()
-#get_sense_mappings()
-#process_sense_annotation()
+
+
+def main():
+    #process_parse_annotation()
+    process_sense_annotation()
+
+if __name__== "__main__":
+    main()
